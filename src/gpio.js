2bc29b57c3e89ae571438fefa81fc08b1bae4e79
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2021-04-19 06:49:30 -0700

    Improve gpio.js

--- src/gpio.js
+++ src/gpio.js
@@ -1,24 +1,30 @@
-const debug = require('debug')('app:gpio');
-const rpio = require('rpio');
+const debug = require("debug")("app:gpio");
+const rpio = require("rpio");
 
 const LED_PIN = parseInt(process.env.LED_PIN);
 const LED_OFF = rpio.HIGH;
 
 const BLINK_SPEED = 500;
-let blinkTimeout, blinkCounter = 0;
+let blinkTimeout,
+    blinkCounter = 0;
 
 function startBlink() {
-    debug('starting blinking');
-    blinkTimeout = setInterval(() => rpio.write(LED_PIN, ++blinkCounter % 2), BLINK_SPEED);
+    debug("starting blinking");
+    blinkTimeout = setInterval(
+        () => rpio.write(LED_PIN, ++blinkCounter % 2),
+        BLINK_SPEED
+    );
 }
 
 function stopBlink() {
-    debug('stopping blinking');
+    debug("stopping blinking");
     clearTimeout(blinkTimeout);
+    blinkCounter = 0;
+    blinkTimeout = null;
     rpio.write(LED_PIN, LED_OFF);
 }
 
-rpio.init({ mapping: 'gpio' });
+rpio.init({ mapping: "gpio" });
 rpio.open(LED_PIN, rpio.OUTPUT, LED_OFF);
 
 module.exports = {
